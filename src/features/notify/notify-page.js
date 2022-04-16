cf90fbd03097f57749012d63b0402f7c2239be9e
muzam1l <muzamilsofi@outlook.com>
2022-04-17 00:34:45 +0530

    mark notifs on notif page as read on visiting the page

--- src/features/notify/notify-page.js
+++ src/features/notify/notify-page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { notifySelectors, readNotif } from './notifySlice'
@@ -25,89 +25,147 @@ export default props => {
     }, [])
 
     const handleClick = n => {
-        if (!n.read)
-            dispatch(readNotif(n))
+        if (!n.read) dispatch(readNotif(n))
     }
-    return (<>
-        <Heading title='Notifications' btnProfile backButton />
-        <ListGroup variant="flush" className="">
-            {notifications.length ? notifications.map(n => {
-                let active = n.read ? '' : 'bg-bg-color border-left-right-primary-custom'
-                let post = n.body.post;
-                let user = n.body.user;
-                let body, heading, anchor = '', tag = n.title
-                switch (n.type) {
-                    case 'mentioned':
-                        anchor = `/post/${post.id_str}`
-                        body = (<div className='d-flex flex-column'>
-                            <p><b>@{post.user.screen_name}</b> mentioned you in post</p>
-                            <blockquote className="bg-light mt-n2 p-2 border-left-right-secondary-custom"><PostText post={post} /></blockquote>
-                        </div>)
-                        break
-                    case 'replied':
-                        anchor = `/post/${post.id_str}`
-                        body = (<div className='d-flex flex-column'>
-                            <p><b>@{post.user.screen_name}</b> replied</p>
-                            <QuotePost post={post} />
-                        </div>)
-                        break
-                    case 'liked':
-                        anchor = `/post/${post.id_str}/likes`
-                        body = (<div className='d-flex flex-column'>
-                            <p><b>@{user.screen_name}</b> liked</p>
-                            <QuotePost post={post} />
-                        </div>)
-                        break
-                    case 'followed':
-                        anchor = `/user/${authUser.screen_name}/followers`
-                        body = (<div className='d-flex flex-column'>
-                            <p><b>@{user.screen_name}</b> started following you</p>
-                        </div>)
-                        break
-                    case 'unfollowed':
-                        anchor = `/user/${user.screen_name}`
-                        body = (<div className='d-flex flex-column'>
-                            <p><b>@{user.screen_name}</b> no longer follows you</p>
-                        </div>)
-                        break
-                    case 'reposted':
-                        anchor = `/post/${post.id_str}/reposts`
-                        body = (<div className='d-flex flex-column'>
-                            <p><b>@{user.screen_name}</b> reposted</p>
-                            <QuotePost post={post} />
-                        </div>)
-                        break
-                    default:
-                        anchor = '/notifications'
-                }
-                if (user) {
-                    heading = (<div className='d-flex'>
-                        <Link to={`/user/${user.screen_name}`}>
-                            <Figure
-                                className="bg-border-color rounded-circle overflow-hidden mr-1 mb-2"
-                                style={{ height: "45px", width: "45px" }}
+
+    const readAll = useCallback(
+        notifications => {
+            notifications.forEach(n => {
+                if (!n.read) dispatch(readNotif(n))
+            })
+        },
+        [dispatch]
+    )
+
+    useEffect(() => {
+        readAll(notifications)
+    }, [notifications, readAll])
+
+    return (
+        <>
+            <Heading title="Notifications" btnProfile backButton />
+            <ListGroup variant="flush" className="">
+                {notifications.length ? (
+                    notifications.map(n => {
+                        let active = n.read ? '' : 'bg-bg-color border-left-right-primary-custom'
+                        let post = n.body.post
+                        let user = n.body.user
+                        let body,
+                            heading,
+                            anchor = '',
+                            tag = n.title
+                        switch (n.type) {
+                            case 'mentioned':
+                                anchor = `/post/${post.id_str}`
+                                body = (
+                                    <div className="d-flex flex-column">
+                                        <p>
+                                            <b>@{post.user.screen_name}</b> mentioned you in post
+                                        </p>
+                                        <blockquote className="bg-light mt-n2 p-2 border-left-right-secondary-custom">
+                                            <PostText post={post} />
+                                        </blockquote>
+                                    </div>
+                                )
+                                break
+                            case 'replied':
+                                anchor = `/post/${post.id_str}`
+                                body = (
+                                    <div className="d-flex flex-column">
+                                        <p>
+                                            <b>@{post.user.screen_name}</b> replied
+                                        </p>
+                                        <QuotePost post={post} />
+                                    </div>
+                                )
+                                break
+                            case 'liked':
+                                anchor = `/post/${post.id_str}/likes`
+                                body = (
+                                    <div className="d-flex flex-column">
+                                        <p>
+                                            <b>@{user.screen_name}</b> liked
+                                        </p>
+                                        <QuotePost post={post} />
+                                    </div>
+                                )
+                                break
+                            case 'followed':
+                                anchor = `/user/${authUser.screen_name}/followers`
+                                body = (
+                                    <div className="d-flex flex-column">
+                                        <p>
+                                            <b>@{user.screen_name}</b> started following you
+                                        </p>
+                                    </div>
+                                )
+                                break
+                            case 'unfollowed':
+                                anchor = `/user/${user.screen_name}`
+                                body = (
+                                    <div className="d-flex flex-column">
+                                        <p>
+                                            <b>@{user.screen_name}</b> no longer follows you
+                                        </p>
+                                    </div>
+                                )
+                                break
+                            case 'reposted':
+                                anchor = `/post/${post.id_str}/reposts`
+                                body = (
+                                    <div className="d-flex flex-column">
+                                        <p>
+                                            <b>@{user.screen_name}</b> reposted
+                                        </p>
+                                        <QuotePost post={post} />
+                                    </div>
+                                )
+                                break
+                            default:
+                                anchor = '/notifications'
+                        }
+                        if (user) {
+                            heading = (
+                                <div className="d-flex">
+                                    <Link to={`/user/${user.screen_name}`}>
+                                        <Figure
+                                            className="bg-border-color rounded-circle overflow-hidden mr-1 mb-2"
+                                            style={{ height: '45px', width: '45px' }}
+                                        >
+                                            <Figure.Image
+                                                src={
+                                                    user.default_profile_image
+                                                        ? '/img/default-profile-vector.svg'
+                                                        : user.profile_image_url_https
+                                                }
+                                                className="w-100 h-100"
+                                            />
+                                        </Figure>
+                                    </Link>
+                                </div>
+                            )
+                        }
+                        return (
+                            <ListGroup.Item
+                                className={`${active} px-lg-5 px-xs-2 px-sm-4`}
+                                action
+                                as={'div'}
+                                key={n._id}
+                                onClick={() => handleClick(n)}
                             >
-                                <Figure.Image
-                                    src={(user.default_profile_image) ? '/img/default-profile-vector.svg' : user.profile_image_url_https}
-                                    className="w-100 h-100"
-                                />
-                            </Figure>
-                        </Link>
-                    </div>)
-                }
-                return <ListGroup.Item
-                    className={`${active} px-lg-5 px-xs-2 px-sm-4`}
-                    action
-                    as={"div"}
-                    key={n._id}
-                    onClick={() => handleClick(n)}
-                >
-                    <Link className="stretched-link" to={anchor}></Link>
-                    <div className='mt-n2 mb-2'><small>{tag}</small></div>
-                    {heading}
-                    {body}
-                </ListGroup.Item>
-            }) : <div className="message font-weight-bold">No notifications yet</div>}
-        </ListGroup>
-    </>)
+                                <Link className="stretched-link" to={anchor}></Link>
+                                <div className="mt-n2 mb-2">
+                                    <small>{tag}</small>
+                                </div>
+                                {heading}
+                                {body}
+                            </ListGroup.Item>
+                        )
+                    })
+                ) : (
+                    <div className="message font-weight-bold">No notifications yet</div>
+                )}
+            </ListGroup>
+        </>
+    )
 }
