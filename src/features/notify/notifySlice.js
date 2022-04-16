0a9762122fac5406dc5f2bbd6538d16c23df3a9d
muzam1l <muzamilsofi@outlook.com>
2022-04-17 00:40:08 +0530

    change

--- src/features/notify/notifySlice.js
+++ src/features/notify/notifySlice.js
@@ -2,14 +2,14 @@ import {
     createAsyncThunk,
     createSlice,
     createEntityAdapter,
-    createSelector
+    createSelector,
 } from '@reduxjs/toolkit'
 // import io from 'socket.io-client'
 import { request } from 'api'
 
 const notifyAdapter = createEntityAdapter({
     selectId: notification => notification._id.toString(),
-    sortComparer: (a, b) => (b.created_at.localeCompare(a.created_at))
+    sortComparer: (a, b) => b.created_at.localeCompare(a.created_at),
 })
 
 // /* TODO move to its own file for general purpose */
@@ -17,12 +17,10 @@ const notifyAdapter = createEntityAdapter({
 //     autoConnect: false,
 //     /*
 //     * As netlify doesn't support socket proxying
-//     * resorting to just polling, instead of using JWT now 
+//     * resorting to just polling, instead of using JWT now
 //     */
 //     transports: ['polling']
 // });
-
-
 
 // export const initSocket = createAsyncThunk(
 //     'notify/initSocket',
@@ -47,38 +45,35 @@ const notifyAdapter = createEntityAdapter({
 const interval = 15 * 1000
 var notifsInterval
 export const fetchNotifs = () => async (dispatch, getState) => {
-    const { auth: { isAuthenticated } } = getState()
+    const {
+        auth: { isAuthenticated },
+    } = getState()
     if (isAuthenticated && !notifsInterval) {
         notifsInterval = setInterval(() => {
             dispatch(fetchNotifs())
         }, interval)
-    }
-    else if (!isAuthenticated && notifsInterval)
-        clearInterval(notifsInterval)
-    const { notify: { status } } = getState()
-    if (status === 'loading')
-        return
+    } else if (!isAuthenticated && notifsInterval) clearInterval(notifsInterval)
+    const {
+        notify: { status },
+    } = getState()
+    if (status === 'loading') return
     dispatch(_fetchNotifs())
 }
 
-export const _fetchNotifs = createAsyncThunk(
-    'notifs/fetchAll',
-    async (_, { dispatch }) => {
-        let { notifications } = await request('/api/notifications', { dispatch })
-        if (!notifications)
-            throw Error('No notifications')
-        return dispatch(notificationsAdded(notifications))
-    }
-)
+export const _fetchNotifs = createAsyncThunk('notifs/fetchAll', async (_, { dispatch }) => {
+    let { notifications } = await request('/api/notifications', { dispatch })
+    if (!notifications) throw Error('No notifications')
+    return dispatch(notificationsAdded(notifications))
+})
 export const readNotif = createAsyncThunk(
     'notifs/readNotif',
     async (notification, { dispatch }) => {
         dispatch(notifRead(notification))
-        return request(`/api/notification_read/${notification._id}`, { dispatch })
+        return request(`/api/notification_read/${notification._id}`, { dispatch, body: {} })
     }
 )
 const initialState = notifyAdapter.getInitialState({
-    status: 'idle' // || 'loading'
+    status: 'idle', // || 'loading'
 })
 const notifySlice = createSlice({
     name: 'notify',
@@ -90,15 +85,21 @@ const notifySlice = createSlice({
             let notif = action.payload
             notifyAdapter.upsertOne(state, {
                 ...notif,
-                read: true
+                read: true,
             })
-        }
+        },
     },
     extraReducers: {
-        [fetchNotifs.pending]: state => { state.status = 'loading' },
-        [fetchNotifs.rejected]: state => { state.status = 'idle' },
-        [fetchNotifs.fulfilled]: state => { state.status = 'idle' }
-    }
+        [fetchNotifs.pending]: state => {
+            state.status = 'loading'
+        },
+        [fetchNotifs.rejected]: state => {
+            state.status = 'idle'
+        },
+        [fetchNotifs.fulfilled]: state => {
+            state.status = 'idle'
+        },
+    },
 })
 
 const { reducer, actions } = notifySlice
@@ -107,7 +108,6 @@ export default reducer
 
 export const notifySelectors = notifyAdapter.getSelectors(state => state.notify)
 
-export const selectUnread = createSelector(
-    [notifySelectors.selectAll],
-    all => all.filter(one => !one.read)
+export const selectUnread = createSelector([notifySelectors.selectAll], all =>
+    all.filter(one => !one.read)
 )
