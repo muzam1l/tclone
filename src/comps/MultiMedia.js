b924019aa855db7d17c9222b198a17642ed304c0
muzam1l <muzamilsofi@outlook.com>
2022-05-01 00:24:34 +0530

    fix safari

--- src/comps/MultiMedia.js
+++ src/comps/MultiMedia.js
@@ -4,41 +4,42 @@ import { ReactTinyLink } from 'react-tiny-link'
 import getUrls from 'get-urls'
 
 function MM(props) {
-    let { post, expanded = false, className } = props;
+    let { post, expanded = false, className } = props
     let style = {
         card: {
-            maxHeight: !expanded ? "350" : "fit-content",
-            overflow: "hidden"
-        }
+            maxHeight: !expanded ? '350' : 'fit-content',
+            overflow: 'hidden',
+        },
     }
     let { entities = {}, text } = post
-    let { media: [photo] = [], urls: [url] } = entities;
+    let {
+        media: [photo] = [],
+        urls: [url],
+    } = entities
     if (photo) {
-        photo = (<Image
-            fluid
-            rounded={true}
-            src={photo.media_url_https}
-            alt='media preview' />
-        )
+        photo = <Image fluid rounded={true} src={photo.media_url_https} alt="media preview" />
     }
     if (!url) {
+        // TODO see if this even necessary
         let unparsed_urls = Array.from(getUrls(text))
         if (unparsed_urls.length) {
             url = {
-                expanded_url: unparsed_urls[0] // just the first one
+                expanded_url: unparsed_urls[0], // just the first one
             }
         }
     }
     if (url) {
-        url = <ReactTinyLink
-            width="100%"
-            cardSize={expanded ? 'large' : "small"}
-            autoPlay={expanded}
-            showGraphic={true}
-            maxLine={2}
-            minLine={1}
-            url={url.expanded_url}
-        />
+        url = (
+            <ReactTinyLink
+                width="100%"
+                cardSize={expanded ? 'large' : 'small'}
+                autoPlay={expanded}
+                showGraphic={true}
+                maxLine={2}
+                minLine={1}
+                url={url.expanded_url}
+            />
+        )
     }
     if (photo || url)
         return (
@@ -47,8 +48,7 @@ function MM(props) {
                 <div className="mt-1">{url}</div>
             </Card>
         )
-    else
-        return <></>
+    else return <></>
 }
 
 export default MM
