1ad31f12a1ba9d3983fb153ed1965651d1c380ce
muzam1l <muzamilsofi@outlook.com>
2022-05-01 00:36:23 +0530

    Add url polyfill

--- config-overrides.js
+++ config-overrides.js
@@ -1,32 +1,20 @@
-const { rewireWorkboxInject, defaultInjectConfig } = require('react-app-rewire-workbox');
+const { rewireWorkboxInject, defaultInjectConfig } = require('react-app-rewire-workbox')
 // const { rewireWorkboxGenerate, defaultGenerateConfig } = require('react-app-rewire-workbox');
-const path = require('path');
+const path = require('path')
 
 module.exports = function override(config, env) {
-    if (env === "production" || true) { // trying for dev also
-        console.log("Adding Workbox for PWAs");
+    if (env === 'production' || true) {
+        // trying for dev also
+        console.log('Adding Workbox for PWAs')
         // Extend the default injection config with required swSrc
         const workboxConfig = {
             ...defaultInjectConfig,
             swSrc: path.join(__dirname, 'src', 'custom-sw.js'),
             maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-        };
-        config = rewireWorkboxInject(workboxConfig)(config, env);
+        }
+        config = rewireWorkboxInject(workboxConfig)(config, env)
     }
+    config.resolve.fallback = { url: require.resolve('url/') }
 
-    return config;
-};
-// module.exports = function override(config, env) {
-//     if (env === "production" || true) {
-//         console.log("Adding Workbox for PWAs");
-//         const workboxConfig = {
-//             ...defaultGenerateConfig,
-//             navigateFallbackBlacklist: undefined,
-//             navigateFallbackDenylist: defaultGenerateConfig.navigateFallbackBlacklist,
-//             maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
-//         };
-//         config = rewireWorkboxGenerate(workboxConfig)(config, env);
-//     }
-
-//     return config;
-// };
+    return config
+}
